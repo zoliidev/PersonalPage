175eb0b18eb531286b34515fb1559395e1e96985
Zolii_ <zoltanvagany058@gmail.com>
2022-12-23 13:49:00 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    let rover = document.getElementById("roverimg");
+    rover.src = "img/rover-windows-xp-still.png";    
+}, 2500);
+
+function easteregg() {
+    var Happy_Mac = new Audio("sound/mac-classic2.wav");
+    Happy_Mac.play();
+  }
