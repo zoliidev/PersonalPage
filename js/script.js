a8b61d61b20a71e3a02f6b5f542ec4f1c5843025
zoltanvagany <mail@vaganyzoltan.hu>
2025-04-26 14:47:25 +0200

    Add welcome page and styling for the application
    - Created a new HTML file for the welcome page with a title and a redirect to the main page after 3 seconds.
    - Added a CSS file to style the welcome page, including layout, colors, and button styles.
    - Implemented responsive design adjustments for better viewing on smaller screens.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function animate() {
+    setTimeout(() => {
+        let rover = document.getElementById("roverimg");
+        rover.src = "img/rover-windows-xp-still.png";
+    }, 2500);
+}
+animate();
+
+function easteregg() {
+    var Happy_Mac = new Audio("sound/mac-classic2.wav");
+    Happy_Mac.play();
+}
+
+let visibility = false;
+
+function toggleMenu() {
+    let menuBtn = document.getElementById("menu");
+    let navBar = document.getElementById("navBar");
+
+    if (visibility) {
+        visibility = false;
+        navBar.style.display = "none";
+        //menuBtn.style.display = "flex";
+    } else {
+        visibility = true;
+        navBar.style.display = "flex";
+        //menuBtn.style.display = "none";
+        animate();
+    }
+}
